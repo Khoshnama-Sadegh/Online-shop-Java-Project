ceb2fa573347719dd94e0984ba0ade9cb7c7ae13
Sadegh <skhoshnama77@gmail.com>
2022-08-30 18:28:24 +0430

    adding context config file

---
+++ src/main/java/com/sadegh/spring/context/Student.java
@@ -0,0 +1,18 @@
+package com.sadegh.spring.context;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+
+    private int age;
+    private String name;
+    private String family;
+
+
+
+}
