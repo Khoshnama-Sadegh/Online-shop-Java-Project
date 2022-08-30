a768b50563774c0b562ce9389004845a14d3ae7e
Sadegh <skhoshnama77@gmail.com>
2022-08-30 16:22:25 +0430

    creating Dummy Class in Session 1 branch

--- src/main/java/com/sadegh/App.java
+++ src/main/java/com/sadegh/App.java
@@ -12,6 +12,16 @@ public class App
 
         System.out.println("Hello sadegh");
 
+        //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
+
+
+
+        //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
+
+
+        //git remote set-url origin https://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@github.com/khoshnama-sadegh/Spring-Course.git
+
+
 
     }
 }
